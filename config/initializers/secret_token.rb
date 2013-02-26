6053476fc56ec4e45d860f935432c819984a0569
Samuel Esposito <samuel.esposito@gmail.com>
2013-02-26 09:04:29 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hospiglu::Application.config.secret_token = 'b4c8ec65bc246490f7b01fdd124ad3d4b02c84938f034d7083b85e4ce607c5bc66375792a456737babad3af0fb6f7a5880148b421b4d581b1a86539d31fd8f31'
