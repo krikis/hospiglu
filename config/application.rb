ffc79bb3b8a0015599df0d92d291246a3504f703
Samuel Esposito <samuel.esposito@gmail.com>
2013-02-26 15:29:46 +0100

    configure generators

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module Hospiglu
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Amsterdam'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :nl
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -58,5 +58,15 @@ module Hospiglu
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.template_engine     :haml
+      g.orm                 :active_record
+      g.test_framework      :rspec, :fixture => false
+      g.fixture_replacement :fabrication
+      g.helper              false
+      g.stylesheets         false
+      g.javascripts         false
+    end
   end
 end
