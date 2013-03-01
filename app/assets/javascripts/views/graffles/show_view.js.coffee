c63f134fb09a91cac11edef0e788353300dd1a2f
Samuel Esposito <samuel.esposito@gmail.com>
2013-03-01 21:02:22 +0100

    restile interface

--- app/assets/javascripts/views/graffles/show_view.js.coffee
+++ app/assets/javascripts/views/graffles/show_view.js.coffee
@@ -3,6 +3,5 @@ Hospiglu.module "Views.Graffles", ->
     template: 'graffles/show'
     onDomRefresh: ->
       graffleProperties = @model.get('properties')
-      @raphael = Raphael('container',
-                         graffleProperties.width,
-                         graffleProperties.height)
+      @raphael = Raphael('menu', '100%', '15%')
+      @raphael = Raphael('container', '100%', '70%')
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,53 +6,57 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-graffle = Graffle.create properties: {name: 'Graffle',
-                                      width: 640,
-                                      height: 480}
+Graffle.destroy_all
+Shape.destroy_all
+Connection.destroy_all
 
-ellipse1 = Shape.create graffle: graffle,
-                        properties: {shape_type: 'ellipse',
-                                     x: 190,
-                                     y: 100,
-                                     width: 30,
-                                     height: 20}
+3.times do
+  graffle = Graffle.create properties: {name: 'Graffle'}
 
-ellipse2 = Shape.create graffle: graffle,
-                        properties: {shape_type: 'ellipse',
-                                     x: 450,
-                                     y: 100,
-                                     width: 20,
-                                     height: 20}
+  ellipse1 = Shape.create graffle: graffle,
+                          properties: {shape_type: 'ellipse',
+                                       x: 190,
+                                       y: 100,
+                                       width: 30,
+                                       height: 20}
 
-rect1 = Shape.create graffle: graffle,
-                     properties: {shape_type: 'rect',
-                                  x: 290,
-                                  y: 80,
-                                  width: 60,
-                                  height: 40,
-                                  border_radius: 10}
+  ellipse2 = Shape.create graffle: graffle,
+                          properties: {shape_type: 'ellipse',
+                                       x: 450,
+                                       y: 100,
+                                       width: 20,
+                                       height: 20}
 
-rect2 = Shape.create graffle: graffle,
-                     properties: {shape_type: 'rect',
-                                  x: 290,
-                                  y: 180,
-                                  width: 60,
-                                  height: 40,
-                                  border_radius: 2}
+  rect1 = Shape.create graffle: graffle,
+                       properties: {shape_type: 'rect',
+                                    x: 290,
+                                    y: 80,
+                                    width: 60,
+                                    height: 40,
+                                    border_radius: 10}
 
-connection1 = Connection.create graffle: graffle,
-                                properties: {line_color: '#fff'},
-                                start_shape: ellipse1,
-                                end_shape: rect1
+  rect2 = Shape.create graffle: graffle,
+                       properties: {shape_type: 'rect',
+                                    x: 290,
+                                    y: 180,
+                                    width: 60,
+                                    height: 40,
+                                    border_radius: 2}
 
-connection2 = Connection.create graffle: graffle,
-                                properties: {line_color: '#fff',
-                                             background_color: '#fff|5'},
-                                start_shape: rect1,
-                                end_shape: rect2
+  connection1 = Connection.create graffle: graffle,
+                                  properties: {line_color: '#fff'},
+                                  start_shape: ellipse1,
+                                  end_shape: rect1
 
-connection2 = Connection.create graffle: graffle,
-                                properties: {line_color: '#000',
-                                             background_color: '#fff'},
-                                start_shape: rect1,
-                                end_shape: ellipse2
+  connection2 = Connection.create graffle: graffle,
+                                  properties: {line_color: '#fff',
+                                               background_color: '#fff|5'},
+                                  start_shape: rect1,
+                                  end_shape: rect2
+
+  connection2 = Connection.create graffle: graffle,
+                                  properties: {line_color: '#000',
+                                               background_color: '#fff'},
+                                  start_shape: rect1,
+                                  end_shape: ellipse2
+end
