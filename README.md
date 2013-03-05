cba4d5c740e92449c4431724ac565ac0efe1d134
Samuel Esposito <samuel.esposito@gmail.com>
2013-03-05 11:56:37 +0100

    update todos

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
 * Create views for 'Department A', 'Department B' and 'Your Department' (coupled to user/sporting depA and depB locked)
 * Add vote view sporting all users Departments
 * Add region for navigating through decision process steps
+* Make sure shapes only persist when dragged below menu
+* Implement destoying shapes/connections
+* Fix mass assignment configuration
--- app/controllers/graffles_controller.rb
+++ app/controllers/graffles_controller.rb
@@ -25,66 +25,6 @@ class GrafflesController < ApplicationController
     end
   end
 
-  # GET /graffles/new
-  # GET /graffles/new.json
-  def new
-    @graffle = Graffle.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @graffle }
-    end
-  end
-
-  # GET /graffles/1/edit
-  def edit
-    @graffle = Graffle.find(params[:id])
-  end
-
-  # POST /graffles
-  # POST /graffles.json
-  def create
-    @graffle = Graffle.new(graffle_params)
-
-    respond_to do |format|
-      if @graffle.save
-        format.html { redirect_to @graffle, notice: 'Graffle was successfully created.' }
-        format.json { render json: @graffle, status: :created, location: @graffle }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @graffle.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /graffles/1
-  # PATCH/PUT /graffles/1.json
-  def update
-    @graffle = Graffle.find(params[:id])
-
-    respond_to do |format|
-      if @graffle.update_attributes(graffle_params)
-        format.html { redirect_to @graffle, notice: 'Graffle was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @graffle.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /graffles/1
-  # DELETE /graffles/1.json
-  def destroy
-    @graffle = Graffle.find(params[:id])
-    @graffle.destroy
-
-    respond_to do |format|
-      format.html { redirect_to graffles_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
     # Use this method to whitelist the permissible parameters. Example:
