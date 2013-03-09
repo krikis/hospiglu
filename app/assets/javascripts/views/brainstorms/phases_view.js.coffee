0c86bd386d7d80c71be126f7a3e931a5c206f517
Samuel Esposito <samuel.esposito@gmail.com>
2013-03-09 17:06:17 +0100

    add menu

--- app/assets/javascripts/views/brainstorms/phases_view.js.coffee
+++ app/assets/javascripts/views/brainstorms/phases_view.js.coffee
@@ -1,3 +1,13 @@
 Hospiglu.module "Views.Brainstorms", ->
   class @PhasesView extends Marionette.ItemView
     template: 'brainstorms/phases'
+
+    triggers:
+      'click .first_department': 'show:firstDepartment'
+      'click .second_department': 'show:secondDepartment'
+
+    onShowFirstDepartment: ->
+      Backbone.history.navigate "brainstorms/first_department", trigger: true
+
+    onShowSecondDepartment: ->
+      Backbone.history.navigate "brainstorms/second_department", trigger: true
--- app/models/graffle.rb
+++ app/models/graffle.rb
@@ -12,6 +12,35 @@ class Graffle < ActiveRecord::Base
 
   serialize :properties
 
+  after_create :init_menu
+
+  def init_menu
+    add_shape shape_type: 'rect', x: 30, y: 30, width: 60, height: 40, border_radius: 10
+    add_shape shape_type: 'rect', x: 130, y: 30, width: 60, height: 40, border_radius: 2
+    add_shape shape_type: 'ellipse', x: 260, y: 50, width: 30, height: 20
+    add_shape shape_type: 'ellipse', x: 350, y: 50, width: 20, height: 20
+    add_connection type: 'curvedpath', stroke: '#fff', target: {:'stroke-width' => 20},
+                   x: 400, y: 30, x2: 440, y2: 70, cx: 400, cy: 50, cx2: 440, cy2: 50
+    add_connection type: 'curvedpath', stroke: '#000', background: {stroke: '#fff', :'stroke-width' => 3},
+                   target: {:'stroke-width' => 20}, x: 480, y: 30, x2: 520, y2: 70, cx: 480, cy: 50, cx2: 520, cy2: 50
+    add_connection type: 'curvedpath', stroke: '#fff', :'stroke-width' => 5, target: {:'stroke-width' => 20},
+                   x: 560, y: 30, x2: 600, y2: 70, cx: 560, cy: 50, cx2: 600, cy2: 50
+    add_connection type: 'trashcan', x: 640, y: 35, size: 2, stroke: '#fff', select_color: '#d14',
+                   target: {type: 'rectangle', x: 630, y: 20, height: 60, width: 50}
+  end
+
+  def add_shape(properties)
+    Shape.create graffle: self,
+                 in_menu: true,
+                 properties: properties
+  end
+
+  def add_connection(properties)
+    Connection.create graffle: self,
+                      in_menu: true,
+                      properties: properties
+  end
+
   def properties
     self[:properties] ||= {}
   end
